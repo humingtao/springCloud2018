3cc2049a47f4e95d26131ecbc8da70e00e478971
humingtao <983086529@qq.com>
2020-04-27 09:20:45 +0800

    Eureka服务注册与发现  --1

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,7 +2,9 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 @SpringBootApplication
 public class DeptProvider8001_App {
     public static void main(String[] args)
